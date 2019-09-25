9d5f203cb3281071388650a96d9863a5eb964465
mindset-tk <yuravian@gmail.com>
2019-09-25 15:53:40 -0700

    YT function: account for lack of arguments

--- commands/yt.js
+++ commands/yt.js
@@ -5,13 +5,19 @@ const youtube = new YouTube(ytAPIkey);
 module.exports = {
 	name: 'yt',
 	aliases: ['youtube'],
-	description: 'Searches youtube and links the first result. If you use a video id it will link directly to the video.',
+	description: 'Searches youtube and links the first result. If you use a video id it will link directly to the video. Performing the command without arguments will link TLO Roll Call.',
 	cooldown: 5,
 	usage: '<search query>',
 	execute(message, args) {
 		async function vidsearch() {
-			const video = await youtube.searchVideos(args.join(' '));
-			message.channel.send('Here is what I found when I searched Youtube for \'' + args.join(' ') + '\': \n' + video.url + ' [' + video.length + ']');
+			if (!args.length) {
+				const video = await youtube.searchVideos('j5y3NQi_RAY');
+				message.channel.send(video.url + ' [' + video.length + ']');
+			}
+			else {
+				const video = await youtube.searchVideos(args.join(' '));
+				message.channel.send('Here is what I found when I searched Youtube for \'' + args.join(' ') + '\': \n' + video.url + ' [' + video.length + ']');
+			}
 		}
 		vidsearch();
 	},
