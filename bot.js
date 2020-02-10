4f552fddfada88ac2512a460ad6a4e3732dd3830
mindset-tk <yuravian@gmail.com>
2020-02-10 11:55:18 -0800

    clean bad variable

--- bot.js
+++ bot.js
@@ -60,7 +60,7 @@ client.on('message', message => {
 	if (command.args && !args.length) {
 		let reply = 'You didn\'t provide any arguments!';
 		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+			reply += `\nThe proper usage would be: \`${config.prefix}${command.name} ${command.usage}\``;
 		}
 		return message.channel.send(reply);
 	}
