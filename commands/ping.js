f4e8ac38e71a66611bf9f7a4e219d7c81df0e5de
mindset-tk <53205683+mindset-tk@users.noreply.github.com>
2020-02-10 12:14:17 -0800

    /

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'ping',
-	aliases: ['ping', 'beep'],
-	description: 'Pings bot to verify operation',
-	cooldown: 3,
-	execute(message, args) {
-		message.channel.send('pong!');
-	},
-};
