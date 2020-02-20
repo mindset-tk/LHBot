f889cf7a6a2304358c49f8133bbfd65e25c72b34
Ian <wererogue@gmail.com>
2020-02-19 21:39:46 -0500

    Concatenate counting messages to be sent at the same time Ban Murray from playing most of the time Added more messages Don't post start over messages when not appropriate on reconnect

--- counting.js
+++ counting.js
@@ -4,6 +4,7 @@ let config = null;
 const countingDataPath = path.resolve('./counting.json');
 const countingData = require(countingDataPath);
 const validCountRegex = /^[0-9]+$/;
+let countingChannel = null;
 
 // Function to write game list data to file
 function WriteState() {
@@ -14,23 +15,21 @@ function WriteState() {
   });
 }
 
-function BuildBotMessage(userMessage, botMessages)
+function BuildBotMessage(author, botMessages)
 {
   const messageIndex = Math.floor(Math.random() * botMessages.length);
   let botMessage = botMessages[messageIndex];
 
-  botMessage = botMessage.replace(/\$user/g, userMessage.author.username);
+  botMessage = botMessage.replace(/\$user/g, author.username);
   return(botMessage);
 }
 
 function FailCounting(message, reason)
 {
-  message.channel.send(BuildBotMessage(message, config.countingFailMessages));
-  message.channel.send(BuildBotMessage(message, config.countingStartMessages));
-  message.channel.send("0");
   //console.log(reason);
   countingData.lastCount = 0;
   countingData.lastMessage = message.id;
+  return BuildBotMessage(message.author, config.countingFailMessages);
 }
 
 function CheckNextMessage(message)
@@ -38,33 +37,53 @@ function CheckNextMessage(message)
   //console.log(message);
   if(!validCountRegex.test(message.content))
   {
-    FailCounting(message, 'Counting failed because invalid attempt: ' + message + ' expected ' + (countingData.lastCount + 1));
-    return;
+    return FailCounting(message, 'Counting failed because invalid attempt: ' + message + ' expected ' + (countingData.lastCount + 1));
   }
 
   const number = parseInt(message.content);
   if(countingData.lastCount != null && number != countingData.lastCount + 1)
   {
-    FailCounting(message, 'Counting failed because out of order: ' + message + ' expected ' + (countingData.lastCount + 1));
-    return;
+    return FailCounting(message, 'Counting failed because out of order: ' + message + ' expected ' + (countingData.lastCount + 1));
   }
 
   countingData.lastCount = number;
   countingData.lastMessage = message.id;
   //console.log(message.content);
+  return null;
 }
 
 function CheckMessages(messages)
 {
   console.log(`Received ${messages.size} messages`);
+  let outputMessages = null;
 
   for(let snowflake of Array.from(messages.keys()).reverse())
   {
     const message = messages.get(snowflake);
     if(!message.author.bot)
     {
-      CheckNextMessage(message);
+      const out = CheckNextMessage(message);
+      if(out)
+      {
+        if(outputMessages)
+        {
+          outputMessages += '\n';
+        }
+        else
+        {
+          outputMessages = "";
+        }
+        outputMessages += out;
+      }
     }
+  }
+  if(outputMessages)
+  {
+    if(countingData.lastCount == 0)
+    {
+      outputMessages += '\n' + BuildBotMessage({author: "dummy"}, config.countingStartMessages);
+    }
+    countingChannel.send(outputMessages);
   }
   console.log(`Resuming counting from ${countingData.lastCount}`);
   WriteState();
@@ -74,7 +93,6 @@ function RestoreCountingState(client)
 {
   console.log('Counting channel: ' + config.countingChannelId);
 
-  var countingChannel;
   while(countingChannel == null)
   {
     console.log('Trying to get Counting channel');
@@ -103,11 +121,11 @@ function InitConfig(lrConfig)
   config = lrConfig;
   if(config.countingFailMessages == null)
   {
-    config.countingFailMessages = ["I think $user broke counting!"];
+    config.countingFailMessages = ["I think $user broke counting!", "That's not right, $user", "Here's $user, ruining it for everyone", "Oh dear, $user. Oh dear.", "It's ok $user, I love you anyway"];
   }
   if(config.countingStartMessages == null)
   {
-    config.countingStartMessages = ["I'll start us off again", "Why not try an easy one?", "Back to the beginning!"];
+    config.countingStartMessages = ["Time to start over", "Back to the beginning!", "Gimme a 1", "What do we start with?", "0"];
   }
 }
 
@@ -121,7 +139,15 @@ function PublicHandleMessage(message)
 {
   if(message.channel.id === config.countingChannelId && !message.author.bot)
   {
-    CheckNextMessage(message);
+    let output = CheckNextMessage(message);
+    if(output)
+    {
+      if(countingData.lastCount == 0)
+      {
+        output += '\n' + BuildBotMessage(message, config.countingStartMessages);
+      }
+      message.channel.send(output);
+    }
     WriteState();
     return(true);
   }
