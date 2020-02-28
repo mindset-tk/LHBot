dbfd5b1464d23f5182bc9fdbdb07caf71b6e85c4
mindset-tk <53205683+mindset-tk@users.noreply.github.com>
2020-02-28 11:50:44 -0800

    fix infinite loop

--- datalog.js
+++ datalog.js
@@ -33,7 +33,7 @@ function publicOnMessage(message) {
   else {
     global.dataLog[message.guild.id][message.channel.id][dateString].numMessages++;
   }
-  if (parseInt(message.id) > parseInt(global.dataLog[message.guild.id][message.channel.id].lastMessageID)) {
+  if ((parseInt(message.id) > parseInt(global.dataLog[message.guild.id][message.channel.id].lastMessageID)) || !global.dataLog[message.guild.id][message.channel.id].lastMessageID) {
     global.dataLog[message.guild.id][message.channel.id].lastMessageID = message.id;
   }
   writeData();
@@ -57,22 +57,49 @@ async function restoreMessages(client) {
         writeData();
       }
       else if (gc.lastMessageID != null && gc.memberPermissions(g.me).has('READ_MESSAGES') && gc.memberPermissions(g.me).has('READ_MESSAGE_HISTORY')) {
-        let lastSeenMessage = global.dataLog[g.id][gc.id].lastMessageID;
-        let numMsgsFetched = 0;
-        while (gc.lastMessageID != lastSeenMessage) {
-          await gc.fetchMessages({ after: lastSeenMessage }).then(messages => {
-            if (messages.size > 0) {
-              for (let message of messages) {
-                message = message[1];
-                publicOnMessage(message);
+        // if the channel doesn't have a null lastMessage, we can just iterate back to the most recent seen message.
+        if (global.dataLog[g.id][gc.id].lastMessageID) {
+          let lastSeenMessage = global.dataLog[g.id][gc.id].lastMessageID;
+          let numMsgsFetched = 0;
+          // fetch messages repeatedly, looping until the guild's last message ID matches our last message ID.
+          while (gc.lastMessageID != lastSeenMessage) {
+            await gc.fetchMessages({ after: lastSeenMessage }).then(messages => {
+              if (messages.size > 0) {
+                for (let message of messages) {
+                  message = message[1];
+                  publicOnMessage(message);
+                }
+                numMsgsFetched += messages.size;
               }
-              numMsgsFetched += messages.size;
-            }
-            lastSeenMessage = global.dataLog[g.id][gc.id].lastMessageID;
+              lastSeenMessage = global.dataLog[g.id][gc.id].lastMessageID;
+            });
             wait(500);
-          });
+          }
+          console.log(`Fetched ${numMsgsFetched} offline messages in #${gc.name}.`);
         }
-        console.log(`Fetched ${numMsgsFetched} offline messages in #${gc.name}.`);
+        // if it was a new channel when we last saw it, we need to do a little more work to iterate back to the first message ever sent, since we don't know the ID of the first message sent.
+        else if (!global.dataLog[g.id][gc.id].lastMessageID) {
+          let oldestSeenMessageID = gc.lastMessageID;
+          let numMsgsFetched = 0;
+          let prevOldest;
+          // loop fetching messages until the oldest seen message no longer changes.
+          do {
+            prevOldest = oldestSeenMessageID;
+            await gc.fetchMessages({ before: oldestSeenMessageID }).then(messages => {
+              if (messages.size > 0) {
+                for (let message of messages) {
+                  message = message[1];
+                  publicOnMessage(message);
+                  oldestSeenMessageID = Math.min(parseInt(message.id),parseInt(oldestSeenMessageID)).toString();
+                }
+                numMsgsFetched += messages.size;
+              }
+            });
+            wait(500);
+          }
+          while (oldestSeenMessageID != prevOldest);
+          console.log(`Fetched ${numMsgsFetched} offline messages in #${gc.name}.`);
+        }
       }
     }
   }
