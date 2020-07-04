50de9ac945de0f9da92749362afdc09b134f00b6
Elliott Frodahl <yuravian@gmail.com>
2020-07-04 12:09:23 -0700

    Reduce yt stutter with larger precache

--- commands/yt.js
+++ commands/yt.js
@@ -125,13 +125,13 @@ If the bot is the only user in a voice channel when it finishes playback of the 
           const dispatcher = connection
             .play(
               ytdl(queue[0].url, {
-              // pass the url to .ytdl()
+                // pass the url to .ytdl()
                 quality: 'highestaudio',
-                // download 10MB of the song before playing it
-                // helps reduces stuttering
-                highWaterMark: 1024 * 1024 * 10,
+                // buffer 32MB prior to playing.
+                highWaterMark: 1024 * 1024 * 32,
               }),
-            )
+              // set the discord.js highWaterMark to 1; it's not needed versus ytdl's and seems to cause some stutter issues.
+              { highWaterMark: 1 })
             .on('start', () => {
               message.guild.musicData.songDispatcher = dispatcher;
               message.guild.musicData.songDispatcher.pausedTime = null;
