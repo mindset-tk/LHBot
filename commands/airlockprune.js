fd57268579f8a7f993e1e55d0a00f1848f20d156
vantiss <>
2020-09-30 13:14:47 -0700

    f'real

--- commands/airlockprune.js
+++ commands/airlockprune.js
@@ -32,10 +32,10 @@ async execute(message, args, client) {
       var outMsg = "";
       
       //Loop through any airlock channels to find the last post date of airlock users
-      airlockChannels = await client.channels.resolve().filter(channel => channel.name.includes(AIRLOCK_CHANNEL) && channel.viewable);
+      airlockChannels = await client.channels.cache.filter(channel => channel.name.includes(AIRLOCK_CHANNEL) && channel.viewable);
       for (channel of airlockChannels) {
         await channel[1].messages.fetch().then(messages => {
-          messages = messages.filter(m => m.member.roles.cache.has(AIRLOCK_ROLE_ID));
+//          messages = messages.filter(m => m.member.roles.cache.has(AIRLOCK_ROLE_ID));
           if (messages.size > 0) {
             for (m of messages) {
               m = m[1];
