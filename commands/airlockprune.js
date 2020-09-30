b42c523b1f3326d7277d56805b463ac649d92a81
vantiss <>
2020-09-29 23:39:28 -0700

    airlockprune now accounts for the last post date of airlockusers too

--- commands/airlockprune.js
+++ commands/airlockprune.js
@@ -16,21 +16,64 @@ module.exports = {
   guildOnly: true,
   staffOnly: true,
   args: false,
-  execute(message, args, client) {
-    if (config.roleAirlock) {
+async execute(message, args, client) {
+    if (config.roleAirlock && config.airlockChannel) {
       const usersUnderLimit = [];
       const usersOverLimit = [];
+      const usrMap = new Map;
       const AIRLOCK_ROLE_ID = config.roleAirlock;
-      const AIRLOCK_PRUNE_LIMIT = config.airlockPruneDays ? config.airlockPruneDays : 0;
+      const AIRLOCK_PRUNE_LIMIT = config.airlockPruneDays ? config.airlockPruneDays : 1;
       const AIRLOCK_PRUNE_KICKMESSAGE = config.airlockPruneMessage ? config.airlockPruneMessage : "Kicked during airlock prune";
-      const canKick = message.guild.me.hasPermission("KICK_MEMBERS");
+      const AIRLOCK_CHANNEL = config.airlockChannel ? config.airlockPruneMessage : false;
+      const currentGuildUsrs = await message.guild.members.cache;
+      const canKick = await message.guild.me.hasPermission("KICK_MEMBERS");
       const pruneNow = Boolean(args[0] == "y");
       const now = moment.utc();
       var outMsg = "";
-      var user = message.guild.roles.cache.get(AIRLOCK_ROLE_ID).members.forEach(u => { 
+      
+      //Loop through any airlock channels to find the last post date of airlock users
+      airlockChannels = await client.channels.cache.filter(channel => channel.name.includes(AIRLOCK_CHANNEL) && channel.viewable);
+      for (channel of airlockChannels) {
+        await channel[1].messages.fetch().then(messages => {
+          messages = messages.filter(m => m.member.roles.cache.has(AIRLOCK_ROLE_ID));
+          if (messages.size > 0) {
+            for (m of messages) {
+              m = m[1];
+              // if the usrmap doesn't have the author at all, add them with value = message ID, so long as they are still currently in the guild
+              if (!usrMap.has(m.author.id) && !m.author.bot && currentGuildUsrs.has(m.author.id)) {
+                usrMap.set(m.author.id, m.id);
+              }
+              // if the usrmap has the author and the msgID stored is less than (older than) the one we're looking at, replace it.
+              if (!m.author.bot && (usrMap.get(m.author.id) < m.id)) {
+                usrMap.set(m.author.id, m.id);
+              }
+            }
+          }
+        });
+      }
+      message.guild.roles.cache.get(AIRLOCK_ROLE_ID).members.forEach(u => { 
         let daysSinceJoin = moment.duration(now.diff(u.joinedAt)).asDays();
-        let entry = `> - <@${u.id}> (${u.id} / ${u.user.username}#${u.user.discriminator}) - joined **${parseInt(daysSinceJoin)} days** ago (${moment(u.joinedAt).format('MMM Do')})`;
-        if (daysSinceJoin >= AIRLOCK_PRUNE_LIMIT) {
+        let hasPosted = usrMap.has(u.id);
+        let entry = `> - <@${u.id}> (${u.user.username}#${u.user.discriminator}) joined `; //${moment(u.joinedAt).format('MMM Do')} displays Feb 23 if we need it
+        if (daysSinceJoin > 1) {
+          entry += `**${parseInt(daysSinceJoin)} day(s)** ago`
+        } else {
+          entry += `**today**`;
+        }
+        if (hasPosted) {
+          var lastPostTimestamp = Number((BigInt(usrMap.get(u.id)) >> BigInt(22)) + BigInt(1420070400000));
+          var timeSinceLastPost = moment.duration(now.diff(lastPostTimestamp)).asDays();
+          if (timeSinceLastPost.asDays > 1) {
+            entry += `, and last posted **${parseInt(timeSinceLastPost)} day(s)** ago`;
+          }
+          else {
+            entry += `, and last posted **today**`;
+          }
+        }
+        else {
+          entry += `, and hasn't posted`;
+        }
+        if (daysSinceJoin >= AIRLOCK_PRUNE_LIMIT && (!hasPosted || timeSinceLastPost >= AIRLOCK_PRUNE_LIMIT)) {
           usersOverLimit.push(entry);
           if (pruneNow && canKick) {
             kickUser(u, AIRLOCK_PRUNE_KICKMESSAGE);
@@ -59,7 +102,7 @@ module.exports = {
       } else { return message.channel.send("There's nobody in the airlock right now!"); }
     }
     else {
-      return message.channel.send("There isn't an airlock role set in the config");
+      return message.channel.send("You need to set both an airlock user role and an airlock channel in the config");
     }
   }
 };
