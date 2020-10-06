4f03441f8c73d5735ffd07096b435aced8579fe0
vantiss <>
2020-10-06 05:28:50 -0700

    step 1 of improved useractivity/prune command, should now be stored to prunedata.json and re-running the command will only iterate through posts since the last check. new file is initialized by bot.js

--- commands/useractivity.js
+++ commands/useractivity.js
@@ -1,6 +1,36 @@
 const wait = require('util').promisify(setTimeout);
+const fs = require('fs');
+const path = require('path');
+const pruneDataPath = path.resolve('./prunedata.json');
+if(global.pruneData == null) {
+  global.pruneData = require(pruneDataPath);
+}
 const ExcelJS = require('exceljs');
-// const path = require('path');
+
+// save disk space and increase readability of datalog.json file.
+function prettyPrintJson() {
+  const output = JSON.stringify(global.pruneData, function(k, v) {
+    if (v instanceof Array) {
+      return JSON.stringify(v);
+    }
+    return v;
+  }, 2).replace(/\\/g, '')
+    .replace(/"\[/g, '[')
+    .replace(/\]"/g, ']')
+    .replace(/"\{/g, '{')
+    .replace(/\}"/g, '}');
+  return output;
+}
+
+// Function to write to .json file for session persistence.
+function writeData() {
+  fs.writeFile(pruneDataPath, prettyPrintJson(), function(err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+}
+
 /* global BigInt */
 
 module.exports = {
@@ -15,9 +45,14 @@ module.exports = {
     if (global.currentlyCaching == 1) {return msg.channel.send('Sorry! I\'m busy with someone else\'s request. Please try again in a few minutes.'); }
     global.currentlyCaching = 1;
     msg.author.send('Please wait a moment while I cache user data...');
+    if (!global.pruneData[msg.guild.id]) {
+      global.pruneData[msg.guild.id] = { guildName: msg.guild.name };
+    }
     const now = new Date();
     const fakeMsgIdNow = (BigInt(now) - BigInt(1420070400000)) << BigInt(22);
-    const usrMap = new Map;
+    const discordEpoch = BigInt(1420070400000);
+    //const usrMap = new Map;
+    const usrMap = global.pruneData[msg.guild.id]['pruneData'] ? new Map(global.pruneData[msg.guild.id]['pruneData']) : new Map();
     for (let g of await client.guilds.cache) {
       g = g[1];
       const currentGuildUsrs = await g.members.cache;
@@ -27,9 +62,16 @@ module.exports = {
           // console.log(gc.name);
           let oldestSeenMessage = fakeMsgIdNow;
           let prevLastSeen;
+          if (!global.pruneData[msg.guild.id]['lastChecked'])
+          {
+            prevLastSeen = (discordEpoch) << BigInt(22);
+          }
+          else {
+            prevLastSeen = global.pruneData[msg.guild.id]['lastChecked'];
+          }
           let oldestMsg = fakeMsgIdNow;
           // fetch messages repeatedly, looping until the channels's last message ID matches our last message ID.
-          while (prevLastSeen != oldestSeenMessage) {
+          while (prevLastSeen > oldestSeenMessage) {
             prevLastSeen = oldestSeenMessage;
             await gc.messages.fetch({ limit: 100, before: oldestSeenMessage }).then(messages => {
               if (messages.size > 0) {
@@ -58,7 +100,7 @@ module.exports = {
         }
       }
     }
-
+    
     const xlsUsrList = new ExcelJS.Workbook;
     const listSheet = xlsUsrList.addWorksheet('User List');
     const colHeaders = ['username', 'lastSeen'];
@@ -68,20 +110,28 @@ module.exports = {
     });
     listSheet.columns = colData;
     // const data = [];
+    
+    
     for (const usr of usrMap) {
       const usrObj = await client.users.fetch(usr[0]);
       let datelastactive = 'N/A';
       if (usr[1] != 0) {
-        const msgUnixDate = (BigInt(usr[1]) >> BigInt(22)) + BigInt(1420070400000);
+        const msgUnixDate = (BigInt(usr[1]) >> BigInt(22)) + discordEpoch;
         // const dateoptions = { timeZone: 'America/Los_Angeles', timeZoneName: 'short', hour: '2-digit', minute: '2-digit', hour12: false };
         datelastactive = new Date(Number(msgUnixDate));
         // data.push(`${usrObj.tag} last message: ${datelastactive.toLocaleString('en-US', dateoptions)}`);
       }
+      
       listSheet.addRow({ username: usrObj.tag, lastSeen: datelastactive });
     }
+    global.pruneData[msg.guild.id]['lastChecked'] = fakeMsgIdNow.toString(); 
     await xlsUsrList.xlsx.writeFile('./usrs.xlsx');
-    // await wait(200);
+    global.pruneData[msg.guild.id].pruneData = [...usrMap];
+    //console.log(global.pruneData);
+    writeData();
+
     msg.author.send({ files: ['./usrs.xlsx'] });
+//    msg.author.send("donezo");
     // msg.channel.send(data.join('\n'));
     global.currentlyCaching = 0;
   },
