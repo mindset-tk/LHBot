f4399aae8e2ecee672bc747090bb7f0a4e143211
helado de brownie <helado.de.brownie@gmail.com>
2021-04-06 23:55:45 -0400

    reformat question.js with eslint

--- commands/question.js
+++ commands/question.js
@@ -1,14 +1,14 @@
-const {start} = require('thoughtful-question-generator')
+const { start } = require('thoughtful-question-generator');
 
 module.exports = {
-    name: 'question',
-    description: 'Generate a thoughtful question.',
-    cooldown: 10,
+  name: 'question',
+  description: 'Generate a thoughtful question.',
+  cooldown: 10,
 
-    execute(message, _arguments, _client, config) {
-        // Are we allowed to respond in this channel?
-        if (config.questionChannelIds.includes(message.channel.id)) {
-            message.channel.send(start.evaluate())
-        }
-    },
-}
+  execute(message, _arguments, _client, config) {
+    // Are we allowed to respond in this channel?
+    if (config.questionChannelIds.includes(message.channel.id)) {
+      message.channel.send(start.evaluate());
+    }
+  },
+};
