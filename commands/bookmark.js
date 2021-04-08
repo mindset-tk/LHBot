70bd46132831334159370e16dde1c7efec594cd6
mindset-tk <yuravian@gmail.com>
2021-04-08 09:51:58 -0700

    edge case for users who have left server

--- commands/bookmark.js
+++ commands/bookmark.js
@@ -25,16 +25,22 @@ exports.init = function(client) {
     if (reaction.emoji.name == '🔖') {
       console.log(`Attempting to PM a message from ${message.channel} to ${user}`);
       const messagesent = new Date(message.createdTimestamp).toLocaleString('en-US', { timeZone: 'UTC' });
-      const guild = message.guild;
-      const guildmember = guild.member(message.author);
       let image = '';
+      let embedAuthor;
+      if (message.member) {
+        embedAuthor = message.member.displayName;
+      }
+      else {
+        embedAuthor = message.author.username;
+      }
+
       if (message.attachments.size > 0) {
         const isimage = /(jpg|jpeg|png|gif)/gi.test((message.attachments.array()[0].url).split('.'));
         if (isimage) { image = message.attachments.array()[0].url; }
       }
       const bookmarkEmbed = new Discord.MessageEmbed()
         .setColor('#0099ff')
-        .setAuthor(guildmember.displayName, message.author.displayAvatarURL())
+        .setAuthor(embedAuthor, message.author.displayAvatarURL())
         .setDescription(message.content + '\n\n [jump to message](' + message.url + ')')
         .setFooter('Bookmarked message was sent at ' + messagesent + ' UTC')
         .setImage(image);
