109565ccbb5c7066064239ab8ee87ea6403d56a5
mindset-tk <yuravian@gmail.com>
2023-10-10 05:13:50 -0700

    New config; Fix PK integration; new common funcs

--- extras/common.js
+++ extras/common.js
@@ -1,45 +1,115 @@
-const path = require('path');
-const configPath = path.resolve('./config.json');
-const config = require(configPath);
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
 
-// function to determine if a user's permission level - returns null, 'comrade', or 'staff'
-function getPermLevel(message) {
-  if (message.channel instanceof Discord.DMChannel) return null;
 
+/**
+* Function to save a specific guild's config tables to the db.
+* @param botdb discord message object
+* @param client discord client
+* @param guildId guild id (optional, but best practice to use it whenever possible to reduce wasteful DB access)
+*
+* @returns {promise} resolves once db writes are complete.
+*/
+async function writeConfigTables(botdb, client, guildId = null) {
+  // if the guildId is specified when calling this function (it should be 99% of the time)
+  if (guildId != null) {
+    const configToWrite = getConfig(client, guildId);
+    const configArr = [];
+    for (const [key, value] of Object.entries(configToWrite)) {
+      configArr.push(botdb.run(`UPDATE config
+        SET value = ?
+        WHERE item = ? AND guild_id =?;`, JSON.stringify(value), key, guildId));
+    }
+    await Promise.all(configArr);
+  }
+  // TODO write an "all guilds" version in case guildId is null.
+}
+
+/**
+* Function to determine if a message author's permission level - returns null, 'member', or 'staff'
+* Use this for messages since they might be PK messages.
+* @param message discord message object
+* @returns {string} null, 'member', or 'staff'
+*/
+function getMessagePermLevel(message) {
+  if (message.channel instanceof Discord.DMChannel) return null;
+  const config = getConfig(message.client, message.guild.id);
+  // if the staff role has not been set, the bot responds to all commands on a server
+  // TODO add some advisory message to this effect - DM the server inviter maybe?
+  if (config.roleStaff == '' || !config.roleStaff) {
+    return 'staff';
+  }
   if (message.isPKMessage) {
-    if (message.PKData.author.roles.cache.has(config.roleStaff)) {
+    if (message.PKData.author.roles.cache.has(config.roleStaff) || message.PKData.author.permissions.has('ADMINISTRATOR')) {
       return 'staff';
     }
-    else if (message.PKData.author.roles.cache.has(config.roleComrade)) {
-      return 'comrade';
+    else if (message.PKData.author.roles.cache.has(config.roleUser)) {
+      return 'user';
     }
     else {return null;}
   }
   else if (!message.isPKMessage && message.member) {
-    if (message.member.roles.cache.has(config.roleStaff)) {
+    if (message.member.roles.cache.has(config.roleStaff) || message.member.permissions.has('ADMINISTRATOR')) {
       return 'staff';
     }
-    else if (message.member.roles.cache.has(config.roleComrade)) {
-      return 'comrade';
+    else if (message.member.roles.cache.has(config.roleUser)) {
+      return 'user';
     }
     else {return null;}
   }
   return null;
 }
 
-// function to create a message collector in a DM. Timeout is 3 minutes.
-async function dmCollector(dmChannel) {
+/**
+* Function to find a user's permission level - returns null, 'member', or 'staff'
+* Use this for eg, Interactions, since a message object is not used here and PK isn't compatible with interactions at this time.
+* @param member discord guildmember object
+* @param guild discord guild object
+* @param client Discord client
+* @returns {string} null, 'member', or 'staff'
+*/
+function getUserPermLevel(member, guild, client) {
+  const config = getConfig(client, guild.id);
+  // if the staff role has not been set, the bot responds to all commands on a server
+  // TODO add some advisory message to this effect - DM the server inviter maybe?
+  if (config.roleStaff == '' || !config.roleStaff) {
+    return 'staff';
+  }
+
+  if (member.roles.cache.has(config.roleStaff) || member.permissions.has('ADMINISTRATOR')) {
+    return 'staff';
+  }
+  else if (member.roles.cache.has(config.roleComrade)) {
+    return 'user';
+  }
+  else {return null;}
+}
+
+// function to get config for a given guild.
+// returns an object with all configurableprops (see config.js)
+function getConfig(client, guildId) {
+  return client.guildConfig.get(guildId);
+}
+
+/**
+* Function to create a message collector in a DM. Default timeout is 3 minutes.
+* Use this for eg, Interactions, since a message object is not used here and PK isn't compatible with interactions at this time.
+* @param dmChannel discord channel object, specifically for a dm
+* @param {integer} timeout optional - timeout in milliseconds to wait for a reply.
+* @returns {string} null, 'member', or 'staff'
+*/
+async function dmCollector(dmChannel, timeout = 18000) {
   // let responses = 0;
   let reply = false;
   // awaitmessages needs a filter but we're just going to accept the first reply it gets.
   const filter = m => (m.author.id === dmChannel.recipient.id);
-  await dmChannel.awaitMessages({ filter, max: 1, time: 180000, errors: ['time'] })
+  await dmChannel.awaitMessages({ filter, max: 1, time: timeout, errors: ['time'] })
     // this method creates a collection; since there is only one entry we get the data from collected.first
     .then(collected => reply = collected.first())
-    .catch(err => dmChannel.send('Sorry, I waited 3 minutes with no response. You will need to start over.'));
-  // console.log('Reply processed...');
+    .catch(err => {
+      console.err(err);
+      dmChannel.send('Sorry, I waited 3 minutes with no response. You will need to start over.');
+    });
   return reply;
 }
 
@@ -52,12 +122,13 @@ async function dmCollector(dmChannel) {
  * Anything else will be returned as the value.
  *
  * @param dmChannel {Discord.DMChannel} The DM channel to prompt in
- * @param handler {function(Discord.Message): object|string|Promise<object|string>}} The function that handles the reply.
+ * @param handler {function(Discord.Message): object|string|Promise<object|string>} The function that handles the reply.
+ * @param timeout (optional) timeout for message prompt
  * @returns {Promise<object|boolean>} Returns the result from the handler or `false` if aborted.
  ` */
-async function promptForMessage(dmChannel, handler) {
+async function promptForMessage(dmChannel, handler, timeout) {
   while (true) {
-    const reply = await dmCollector(dmChannel);
+    const reply = await dmCollector(dmChannel, (timeout || null));
     if (!reply) {
       return false;
     }
@@ -83,7 +154,7 @@ async function promptForMessage(dmChannel, handler) {
  *
  * @param dmChannel {Discord.DMChannel} The DM channel to prompt in
  * @param options {object} Options containing messages with strings for the above keys.
- * @returns {Promise<object|boolean>} Returns `{answer: true}` for yes, `{answer: true}` for no, or `false` if aborted.
+ * @returns {Promise<object|boolean>} Returns `{answer: true}` for yes, `{answer: false}` for no, or `false` if aborted.
  */
 async function promptYesNo(dmChannel, options) {
   return promptForMessage(dmChannel, (reply) => {
@@ -121,6 +192,7 @@ async function pkQuery(message, force = false) {
     message.PKData = {};
   }
   if (!message.author.bot) {
+    message.isPKMessage = false;
     message.PKData = {
       author: null,
       system: null,
@@ -129,10 +201,10 @@ async function pkQuery(message, force = false) {
     return message.PKData;
   }
   if (!force && message.pkCached) return message.PKData;
-  const pkAPIurl = 'https://api.pluralkit.me/v1/msg/' + message.id;
+  const pkAPIurl = 'https://api.pluralkit.me/v2/messages/' + message.id;
   try {
     let pkResponse = await fetch(pkAPIurl);
-    if (pkResponse.headers.get('content-type').includes('application/json')) {
+    if (pkResponse.ok && pkResponse.headers.get('content-type').includes('application/json')) {
       message.isPKMessage = true;
       pkResponse = await pkResponse.json();
       try { message.PKData.author = await message.guild.members.fetch(pkResponse.sender);}
@@ -161,10 +233,62 @@ async function pkQuery(message, force = false) {
   return message.PKData;
 }
 
+/**
+* Convenience function for checking if a channel is a valid guild text channel.
+*
+* @param channel {Discord.Channel} The channel to check.
+* @returns {boolean} Returns true if the channel is a guild text channel, false if not.
+*/
+function isTextChannel(channel) {
+  if (channel.type.toLowerCase().includes('guild') && channel.isText()) {
+    return true;
+  }
+  return false;
+}
+
+const emojiRegex = require('emoji-regex');
+const unicodeEmojiTest = emojiRegex();
+const discordEmojiTest = new RegExp(/<a?:.+?:(\d+)>$/);
+
+/**
+* Test if input is a unicode or Discord emoji mention, and if it can be posted by the bot (eg, is not animated or from a different server.)
+* @param message Discord message object
+* @param emoji string to test
+*
+* @returns postable emoji string, if emoji can be posted by the bot, false if not.
+*/
+async function validateEmoji(client, emoji) {
+  // first check if the string contains exactly one unicode emoji and nothing more.
+  if (emoji.match(unicodeEmojiTest)) {
+    if (emoji == emoji.match(unicodeEmojiTest)[0] && emoji.match(unicodeEmojiTest).length == 1) {
+      return emoji;
+    }
+  }
+  // if not, test if it's a single custom emoji that the bot can access.
+  try {
+    if (discordEmojiTest.test(emoji)) {
+      emoji = await client.emojis.resolve(discordEmojiTest.exec(emoji)[1]);
+      if (emoji && emoji.available) {
+        return `<${emoji.animated ? 'a' : ''}:${emoji.name}:${emoji.id}>`;
+      }
+    }
+  }
+  catch (err) {
+    console.error(err.message);
+    return false;
+  }
+  return false;
+}
+
 module.exports = {
-  getPermLevel,
+  getMessagePermLevel,
+  getUserPermLevel,
   dmCollector,
   promptForMessage,
   promptYesNo,
   pkQuery,
+  getConfig,
+  writeConfigTables,
+  isTextChannel,
+  validateEmoji,
 };
